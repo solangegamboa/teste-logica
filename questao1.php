6a53cd2fe42de813120063d943052e78dd3894ca
System Administrator <root@macbook.local>
2016-01-06 23:45:25 -0200

    bootstrap

--- questao1.php
+++ questao1.php
@@ -1,21 +1,41 @@
-<?php
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Teste Lógica</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <style type="text/css">
+      body {
+      	margin:0;
+      	padding:0;
+      	text-align:center;
+      	}
+    </style>
+  </head>
+  <body>
+    <div class="">
+      <h2>Resultado Questão 1 </h2>
+        <?php
 
-$numero1 = $_POST['numero1'];
-$numero2 = $_POST['numero2'];
+        $numero1 = $_POST['numero1'];
+        $numero2 = $_POST['numero2'];
 
-function verificaZero($num){
-  if($num == 0){
-    echo
-      "<p>Segundo numero não pode ser Zero</p><br>
-      <a href='index.html'>voltar</a>";
-      return false;
-  }else{
-    return $num;
-  }
-}
+        function verificaZero($num){
+          if($num == 0){
+            echo
+              "<p>Segundo numero não pode ser Zero</p><br>
+              <a href='index.html'>voltar</a>";
+              return false;
+          }else{
+            return $num;
+          }
+        }
 
-if(verificaZero($numero2) != false){
-  $divisao = $numero1 / $numero2;
-  echo "<p>Resultado da Divisão: ".$divisao."</p>";
-  echo "<br><a href='index.html'>Voltar</a>";
-}
+        if(verificaZero($numero2) != false){
+          $divisao = $numero1 / $numero2;
+          echo "<p>Resultado da Divisão: ".$divisao."</p>";
+          echo "<br><a href='index.html'>Voltar</a>";
+        }
+        ?>
+  </body>
+</html>
--- questao2.php
+++ questao2.php
@@ -1,25 +1,44 @@
-<?php
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Teste Lógica</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <style type="text/css">
+      body {
+      	margin:0;
+      	padding:0;
+      	text-align:center;
+      	}
+    </style>
+  </head>
+  <body>
+    <div class="">
+      <h2>Resultado Questão 2</h2>
+      <?php
+      function verificaString($str){
+        $exist = array();
 
-function verificaString($str){
-  $exist = array();
-
-  for($i=0; $i<strlen($str); $i++){
-      $count=0;
-      if(in_array($str[$i],$exist)) continue;
-      $exist[]=$str[$i];
-      for($j=$i+1;$j<strlen($str);$j++) {
-          if($str[$i]==$str[$j]) {
-              $count=1;
-              break;
-          }
+        for($i=0; $i<strlen($str); $i++){
+            $count=0;
+            if(in_array($str[$i],$exist)) continue;
+            $exist[]=$str[$i];
+            for($j=$i+1;$j<strlen($str);$j++) {
+                if($str[$i]==$str[$j]) {
+                    $count=1;
+                    break;
+                }
+            }
+            if($count!=1){
+                echo "<p>Primeiro caractere unico de ".trim($str)." é: ".$str[$i]."</p>";
+                echo "<br><a href='index.html'>Voltar</a>";
+                break;
+            }
+        }
       }
-      if($count!=1)
-      {
-          echo "<p>Primeiro caractere unico de ".trim($str)." é: ".$str[$i]."</p>";
-          echo "<br><a href='index.html'>Voltar</a>";
-          break;
-      }
-  }
-}
 
-verificaString(trim($_POST['string']));
+      verificaString(trim($_POST['string']));
+      ?>
+    </div>
+  </body>
+</html>
