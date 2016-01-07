5921331fd64ef6cd756d52327ca1ad5e7cd8b818
System Administrator <root@macbook.local>
2016-01-06 23:30:13 -0200

    interface grafica

--- questao1.php
+++ questao1.php
@@ -1,26 +1,21 @@
 <?php
-print_r($_POST);
 
 $numero1 = $_POST['numero1'];
 $numero2 = $_POST['numero2'];
+
 function verificaZero($num){
   if($num == 0){
     echo
       "<p>Segundo numero não pode ser Zero</p><br>
-      <a href='index.php'>voltar</a>";
+      <a href='index.html'>voltar</a>";
+      return false;
   }else{
     return $num;
   }
 }
 
-$divisao = $numero1 / verificaZero($numero2);
-echo "<p> Resultado da divisão = ".$divisao."</p>";
-/*echo "Entre com o primeiro número: ";
-$handle1 = fopen ("php://stdin","r");
-$num1 = fgets($handle1);
-
-echo "Entre com o segundo numero número: ";
-$handle2 = fopen ("php://stdin","r");
-$num2 = verificaZero(fgets($handle2));
-$divisao = $num1 / $num2;
-echo "Resultado da divisão: ".$divisao."\n";*/
+if(verificaZero($numero2) != false){
+  $divisao = $numero1 / $numero2;
+  echo "<p>Resultado da Divisão: ".$divisao."</p>";
+  echo "<br><a href='index.html'>Voltar</a>";
+}
--- questao2.php
+++ questao2.php
@@ -1,23 +1,25 @@
 <?php
 
-echo "Entre com a string: ";
-$handle = fopen ("php://stdin","r");
-$str = fgets($handle);
-$exist = array();
+function verificaString($str){
+  $exist = array();
 
-for($i=0; $i<strlen($str); $i++){
-    $count=0;
-    if(in_array($str[$i],$exist)) continue;
-    $exist[]=$str[$i];
-    for($j=$i+1;$j<=strlen($str);$j++) {
-        if($str[$i]==$str[$j]) {
-            $count=1;
-            break;
-        }
-    }
-    if($count!=1)
-    {
-        echo "Primeiro caractere unico de ".trim($str)." é: ".$str[$i]."\n";
-        break;
-    }
+  for($i=0; $i<strlen($str); $i++){
+      $count=0;
+      if(in_array($str[$i],$exist)) continue;
+      $exist[]=$str[$i];
+      for($j=$i+1;$j<strlen($str);$j++) {
+          if($str[$i]==$str[$j]) {
+              $count=1;
+              break;
+          }
+      }
+      if($count!=1)
+      {
+          echo "<p>Primeiro caractere unico de ".trim($str)." é: ".$str[$i]."</p>";
+          echo "<br><a href='index.html'>Voltar</a>";
+          break;
+      }
+  }
 }
+
+verificaString(trim($_POST['string']));
